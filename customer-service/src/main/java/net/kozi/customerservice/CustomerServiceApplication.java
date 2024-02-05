a2eb20cb90d3f32946cd0e1193f5c2a488f35fe0
kozi-corner <abdelhamid.taoufiqallah@gmail.com>
2024-02-05 03:35:05 +0100

    APP_V1

--- customer-service/src/main/java/net/kozi/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/net/kozi/customerservice/CustomerServiceApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import java.util.List;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(GlobalConfig.class)
+@EnableConfigurationProperties(GlobalConfig.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
